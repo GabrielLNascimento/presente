e6bdcae3f880d15d486f24c87f9e6bb51f13271c
Gabriel De Liz <gabriellizcfnpbnu@gmail.com>
2024-06-11 19:21:39 -0300

    upload files

---
+++ script/script.js
@@ -0,0 +1,5 @@
+function respostaErrada() {
+    alert('Ops... Respota errada')
+    window.location.href = 'file:///I:/projetos%20pessoais/presente-2024/fases/quiz/resposta-errada.html'
+    window.location.assign('file:///I:/projetos%20pessoais/presente-2024/fases/quiz/resposta-errada.html')
+}
